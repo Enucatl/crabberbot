20ac299525830a6bff9a487161d5c0de5d021cab
Matteo Abis <1423701+Enucatl@users.noreply.github.com>
2025-06-26 18:01:25 +0200

    create tunnel with cloudflare

---
+++ cloudflare-terraform/cloudflare.tf
@@ -0,0 +1,38 @@
+# Creates a new remotely-managed tunnel for the crabberbot VM.
+resource "cloudflare_zero_trust_tunnel_cloudflared" "crabberbot_tunnel" {
+  account_id    = var.cloudflare_account_id
+  name          = "Terraform crabberbot tunnel"
+}
+
+# Reads the token used to run the tunnel on the server.
+data "cloudflare_zero_trust_tunnel_cloudflared_token" "crabberbot_tunnel_token" {
+  account_id   = var.cloudflare_account_id
+  tunnel_id   = cloudflare_zero_trust_tunnel_cloudflared.crabberbot_tunnel.id
+}
+
+# Creates the CNAME record that routes crabberbot.${var.cloudflare_zone} to the tunnel.
+resource "cloudflare_dns_record" "crabberbot" {
+  zone_id = var.cloudflare_zone_id
+  name    = "crabberbot"
+  content = "${cloudflare_zero_trust_tunnel_cloudflared.crabberbot_tunnel.id}.cfargotunnel.com"
+  type    = "CNAME"
+  ttl     = 1
+  proxied = true
+}
+
+# Configures tunnel with a public hostname route for clientless access.
+resource "cloudflare_zero_trust_tunnel_cloudflared_config" "crabberbot_tunnel_config" {
+  tunnel_id  = cloudflare_zero_trust_tunnel_cloudflared.crabberbot_tunnel.id
+  account_id = var.cloudflare_account_id
+  config     = {
+    ingress   = [
+      {
+        hostname = "crabberbot.${var.cloudflare_zone}"
+        service  = "http://crabberbot:8080"
+      },
+      {
+        service  = "http_status:404"
+      }
+    ]
+  }
+}
