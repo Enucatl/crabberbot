f992beb5f8156391826c11d99cb24dfb7f0cb6af
Matteo Abis <1423701+Enucatl@users.noreply.github.com>
2025-06-27 22:53:29 +0200

    add output

--- cloudflare-terraform/outputs.tf
+++ cloudflare-terraform/outputs.tf
@@ -3,3 +3,9 @@ output "tunnel_token" {
   value       = data.cloudflare_zero_trust_tunnel_cloudflared_token.crabberbot_tunnel_token.token
   sensitive   = true 
 }
+
+output "test_tunnel_token" {
+  description = "The token for the test Cloudflare Tunnel. Use this with `cloudflared tunnel run --token`."
+  value       = data.cloudflare_zero_trust_tunnel_cloudflared_token.crabberbot_test_tunnel_token.token
+  sensitive   = true 
+}
